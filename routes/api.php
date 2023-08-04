f4ca0bbe5cb61b19d77b9deaeefdb5eb112a3440
codeartisan93 <narutoninja815@hotmail.com>
2023-08-04 20:38:24 +0900

    minor bug fix and laravel/ui for vue added

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('api')->group(function () {
     Route::resource('products', ProductController::class);
-})
+});
